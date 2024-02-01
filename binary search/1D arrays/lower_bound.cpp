e41d449c797a4dfa1af188f24d3d68c76045fd25
Ayush Rakesh <ayushrakesh35711artlover@gmail.com>
2024-02-01 18:43:55 +0530

    Lower bound and Upper bound implementation

---
+++ binary search/1D arrays/lower_bound.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> v, int target)
+{
+  int n = v.size();
+  int low = 0, high = n - 1;
+  int ans = n;
+  while (low <= high)
+  {
+    /* code */
+
+    int mid = (low + high) / 2;
+
+    if (v[mid] >= target)
+    {
+      ans = mid;
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> v;
+  int a = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a;
+    v.push_back(a);
+  }
+  int target = 0;
+  cin >> target;
+  cout << lowerBound(v, target) << endl;
+}
