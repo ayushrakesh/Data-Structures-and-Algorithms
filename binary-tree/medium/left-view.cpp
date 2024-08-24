13c081b9bb9863cc5a537dd2484986c611e147b9
ayushrakesh <ayushrakesh.05@gmail.com>
2024-08-24 15:38:55 +0530

    Left and right side view of tree

---
+++ binary-tree/medium/left-view.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int d)
+  {
+    this->val = d;
+    left = right = nullptr;
+  }
+};
+void solve(TreeNode *root, vector<int> &res, int level)
+{
+  if (root == NULL)
+    return;
+
+  if (level == res.size())
+  {
+    res.push_back(root->val);
+  }
+  solve(root->left, res, level + 1);
+  solve(root->right, res, level + 1);
+}
+vector<int> rightSideView(TreeNode *root)
+{
+  vector<int> res;
+  int level = 0;
+  solve(root, res, level);
+  return res;
+}
