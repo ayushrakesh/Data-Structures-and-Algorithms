d92dde5f24b14a1074c4890cb9c0d28f874c737e
ayushrakesh <ayushrakesh.05@gmail.com>
2024-10-11 22:49:15 +0530

    Dikshtras algo with set and priority queue

--- graph/shortest-path-algos/shortest-path-undirected.cpp
+++ graph/shortest-path-algos/shortest-path-undirected.cpp
@@ -1,55 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+vector<int> solveSet(int V, vector<vector<int>> adj[], int S)
 {
-public:
-  vector<int> shortestPath(vector<vector<int>> &edges, int N, int M, int src)
+  vector<int> res(V, INT_MAX);
+  set<pair<int, int>> s;
+  s.insert({0, S});
+  res[S] = 0;
+
+  while (!s.empty())
   {
-    // code here
-    vector<int> v(N, 0);
-    vector<int> adj[N];
+    auto ite = *(s.begin());
+    int node = ite.second;
+    int dis = ite.first;
 
-    for (int i = 0; i < M; i++)
+    s.erase(ite);
+
+    for (auto it : adj[node])
     {
-      adj[edges[i][0]].push_back(edges[i][1]);
-      adj[edges[i][1]].push_back(edges[i][0]);
-    }
-    queue<pair<int, int>> q;
+      int nei = it[0];
+      int wei = it[1];
 
-    q.push({src, 0});
-    v[src] = 1;
-    vector<int> res(N, INT_MAX);
-    int cnt = 1;
-    res[src] = 0;
-
-    while (!q.empty())
-    {
-
-      int node = q.front().first;
-      int dis = q.front().second;
-      q.pop();
-
-      for (auto it : adj[node])
+      if (res[nei] == INT_MAX)
       {
-        if (!v[it])
+        res[nei] = wei + res[node];
+        s.insert({res[nei], nei});
+      }
+      else
+      {
+        if (wei + res[node] < res[nei])
         {
-          v[it] = 1;
-          res[it] = dis + 1;
-          q.push({it, dis + 1});
-        }
-        else
-        {
-          res[it] = min(res[it], dis + 1);
+          s.erase({res[nei], nei});
+          res[nei] = wei + res[node];
+          s.insert({res[nei], nei});
         }
       }
     }
-
-    for (int i = 0; i < N; i++)
-    {
-      if (res[i] == INT_MAX)
-        res[i] = -1;
-    }
-    return res;
   }
-};
+  return res;
+}
+vector<int> shortestPath(vector<vector<int>> &edges, int N, int M, int src)
+{
+  // code here
+  vector<int> v(N, 0);
+  vector<int> adj[N];
+
+  for (int i = 0; i < M; i++)
+  {
+    adj[edges[i][0]].push_back(edges[i][1]);
+    adj[edges[i][1]].push_back(edges[i][0]);
+  }
+  queue<int> q;
+
+  q.push({src});
+  v[src] = 1;
+  vector<int> res(N, INT_MAX);
+  // int cnt=1;
+  res[src] = 0;
+
+  while (!q.empty())
+  {
+
+    int node = q.front();
+    // int dis=q.front().second;
+    q.pop();
+
+    for (auto it : adj[node])
+    {
+      if (!v[it])
+      {
+        v[it] = 1;
+        q.push({it});
+        res[it] = res[node] + 1;
+      }
+      else
+      {
+        res[it] = min(res[it], res[node] + 1);
+      }
+    }
+  }
+
+  for (int i = 0; i < N; i++)
+  {
+    if (res[i] == INT_MAX)
+      res[i] = -1;
+  }
+  return res;
+}
