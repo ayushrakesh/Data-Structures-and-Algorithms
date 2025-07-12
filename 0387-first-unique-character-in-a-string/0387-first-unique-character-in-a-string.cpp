80e84645d042d995587dd90e243ac209fe8a53bb
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-07-12 21:06:54 +0530

    Time: 3 ms (83.78%), Space: 14.7 MB (60.13%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> freq(26, 0);
+    
+        for (auto& c : s) {
+            freq[c - 'a']++;
+        }
+        
+        int i = 0;
+        for (auto& c : s) {
+            if (freq[c - 'a'] == 1) {
+                return i;
+            }
+            i++;
+        }
+        
+        return -1;
+    }        
+};
