9f697c29c192ce8bedc35bce7b6031efab40289d
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-07-18 12:03:56 +0530

    Time: 0 ms (100%), Space: 33.1 MB (89.95%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -1,15 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
- TreeNode*rec(TreeNode*r1, TreeNode*r2){
-      if(!r1 and !r2) return NULL;
-      if (!r1) return r2;
-      if (!r2) return r1;
-      r1->val+=r2->val;
-      r1->left=rec(r1->left,r2->left);
-      r1->right=rec(r1->right,r2->right);
-     return r1;
-   }
+    TreeNode*solve(TreeNode*a,TreeNode*b){
+        if(!a)return b;
+        if(!b)return a;
+        
+        b->val +=a->val;
+
+        b->left = solve(a->left,b->left);
+        b->right = solve(a->right,b->right);
+
+        return b;
+
+    }
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        return rec(root1, root2);
+        return solve(root1,root2);
     }
 };
