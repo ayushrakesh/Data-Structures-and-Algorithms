f2582c8771b384dfd3117c52e698039e51daf120
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-07-21 12:13:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (33.07%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
+    bool brute(string s, string t) {
+        int ind=0;
+
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            bool b = false;
+            for (int j = ind ; j < t.size(); j++) {
+                if (t[j] == c) {
+                    ind = j+1;
+                    b = true;
+                    break;
+                }
+            }
+            if (!b)
+                return false;
+        }
+        return true;
+    }
+    bool optimal(string s,string t){
         int sp = 0;
         int tp = 0;
 
@@ -10,7 +28,10 @@ public:
             }
             tp++;
         }
-
-        return sp == s.length();        
+        return sp == s.length();
+    }
+    bool isSubsequence(string s, string t) {
+        // return optimal(s,t);
+        return brute(s,t);
     }
 };
