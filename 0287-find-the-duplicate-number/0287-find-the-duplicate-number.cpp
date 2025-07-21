08a19bf5815139767d58b883dcbbcf87e338b9d9
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-07-21 16:25:35 +0530

    Time: 3 ms (68.74%), Space: 65.1 MB (42.18%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
-    void cyclicSort(vector<int>& v) {
-        int i = 0;
-        while (i < v.size()) {
-            int corr = v[i]-1;
-
-            if (v[i] != v[corr])
-                swap(v[i], v[corr]);
-            else
-                i++;
-        }
-    }
-    int slowAndFast(vector<int>&nums){
+    void cyclicsort(vector<int>&nums){
         int n=nums.size();
-
-        int slow=nums[0];
-        int fast=nums[0];
-
-        while(true){
-            slow=nums[slow];
-            fast=nums[nums[fast]];
-
-            if(slow==fast)break;
+        int i=0;
+        while(i<n){
+            int corr=nums[i]-1;
+            if(nums[i] != nums[corr])swap(nums[i],nums[corr]);
+            else i++;
         }
-
-        slow=nums[0];
-
-        while(slow != fast){
-            slow=nums[slow];
-            fast=nums[fast];
-        }
-
-        return fast;
     }
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        cyclicSort(nums);
-
-        return slowAndFast(nums);
+        cyclicsort(nums);
+        return nums[nums.size()-1];
     }
 };
