144fb3e50029b291d506b32995d1e1e9e48ac05d
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-07-21 16:57:05 +0530

    Time: 0 ms (100%), Space: 65.1 MB (69.71%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -9,8 +9,43 @@ public:
             else i++;
         }
     }
+    int optimal(vector<int>&nums){
+        int n=nums.size();
+        
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow != fast);
+
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return fast;
+    }
+    int better(vector<int>&nums){
+        int n=nums.size();
+        int low=1,high=n;
+
+        while(low<high){
+            int mid=low+((high-low)/2);
+            int cnt=0;
+            for(int num:nums){
+                if(num <= mid)cnt++;
+            }
+
+            if(cnt<=mid)high=mid;
+            else low=mid+1;
+        }
+        return low;
+    }
     int findDuplicate(vector<int>& nums) {
-        cyclicsort(nums);
-        return nums[nums.size()-1];
+        // cyclicsort(nums);
+        // return optimal(nums);
+        // return better(nums);
+        return optimal(nums);
     }
 };
