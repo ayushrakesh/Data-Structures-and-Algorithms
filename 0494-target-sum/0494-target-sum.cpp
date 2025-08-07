c653d6e61d00196e9a7bb0788cacbc1a77ffd262
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-08-07 22:28:30 +0530

    Time: 16 ms (50.02%), Space: 41.2 MB (18.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int solve(int ind, int curr, vector<int>& nums, int target, int& res,vector<vector<int>>&dp) {
-        if (ind >= nums.size()) {
-            if (curr == target) return 1;
-            return 0;
+    int recur(vector<int>& nums, int target, int curr, int ind) {
+        int n = nums.size();
+        if (ind >= n) {
+            return curr == target ? 1 : 0;
         }
 
-        if(dp[ind][curr+1000] != 0) return dp[ind][curr+1000]; 
+        int plus = recur(nums, target, curr + nums[ind], ind + 1);
+        int minus = recur(nums, target, curr - nums[ind], ind + 1);
 
-        dp[ind][curr+1000]+= solve(ind + 1, curr + nums[ind], nums, target, res,dp);
-        dp[ind][curr+1000]+= solve(ind + 1, curr - nums[ind], nums, target, res,dp);
-        
-        return dp[ind][curr+1000];
+        return plus + minus;
     }
+    int memo(vector<int>& nums, int target, int curr, int ind,
+             vector<vector<int>>& dp) {
+        int n = nums.size();
+        if (ind >= n) {
+            return curr == target ? 1 : 0;
+        }
 
+        if (dp[ind][curr + 1000] != -1)
+            return dp[ind][curr + 1000];
+
+        int plus = memo(nums, target, curr + nums[ind], ind + 1, dp);
+        int minus = memo(nums, target, curr - nums[ind], ind + 1, dp);
+
+        return dp[ind][curr + 1000] = plus + minus;
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int res = 0;
-        vector<vector<int>> dp(nums.size(),vector<int>(2001,0));
-        return solve(0, 0, nums, target, res,dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(2001, -1));
+        return memo(nums, target, 0, 0, dp);
     }
 };
