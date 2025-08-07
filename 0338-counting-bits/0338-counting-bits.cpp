4dea7345016533d5d8969954c413f33c5dba21ba
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-08-08 01:57:09 +0530

    Time: 3 ms (25.99%), Space: 11 MB (35.43%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -8,12 +8,22 @@ public:
         }
         return cnt;
     }
+    int recur(int n,vector<int>&dp){
+        if(n==0)return 0;
+        if(n==1)return 1;
+
+        if(dp[n] != -1)return dp[n];
+
+        return dp[n] = recur(n >> 1,dp)+(n&1);
+    }
     vector<int> countBits(int n) {
         vector<int> res(n + 1, -1);
+        vector<int> dp(n+1,-1);
 
         for (int i = 0; i <= n; i++) {
-            res[i] = countsetbits(i);
+            res[i] = recur(i,dp);
         }
+        // return res;
         return res;
     }
 };
