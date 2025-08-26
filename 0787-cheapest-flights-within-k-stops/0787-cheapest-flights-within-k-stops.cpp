adcc7d421c9f096a15a0a9fe72aa87d5fdcfec51
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-08-26 17:11:32 +0530

    Time: 6 ms (29.44%), Space: 17.8 MB (76.98%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,34 +2,38 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
                           int k) {
-        int m = flights.size();
-        vector<pair<int, int>> adj[n];
-        for (int i = 0; i < m; i++) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (int i = 0; i < flights.size(); i++) {
             adj[flights[i][0]].push_back({flights[i][1], flights[i][2]});
         }
-        vector<int> res(n, INT_MAX);
-        queue<pair<int, pair<int, int>>> q;
-        q.push({0, {src, 0}});
-        res[src] = 0;
+        vector<int> dis(n, INT_MAX);
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
 
-        while (!q.empty()) {
-            int stops = q.front().first;
-            int node = q.front().second.first;
-            int dis = q.front().second.second;
-            q.pop();
+        pq.push({0, {0, src}});
+        dis[src] = 0;
 
-            for (auto it : adj[node]) {
-                int nei = it.first;
-                int wei = it.second;
+        while (!pq.empty()) {
+            int node = pq.top().second.second;
+            int stops = pq.top().first;
+            int d = pq.top().second.first;
+            pq.pop();
+            if (stops <= k) {
+                // cout<<node<<" "<<stops<<endl;
 
-                if (wei + dis < res[nei] && stops <= k) {
-                    res[nei] = wei + dis;
-                    q.push({stops + 1, {nei, res[nei]}});
+                for (auto it : adj[node]) {
+                    int nei = it.first;
+                    int wt = it.second;
+
+                    if (dis[nei] > (d + wt)) {
+                        dis[nei] = d + wt;
+                        pq.push({stops + 1, {dis[nei], nei}});
+                    }
                 }
             }
         }
-        if (res[dst] == INT_MAX)
-            return -1;
-        return res[dst];
+        return dis[dst] == INT_MAX ? -1 : dis[dst];
     }
 };
