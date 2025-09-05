a800b20fcbaaaa5abb0f0c8cbfc8a0547a9aaae3
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-09-05 20:30:57 +0530

    Time: 83 ms (5.54%), Space: 37.5 MB (5.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> better(vector<string>& strs) {
+        int n = strs.size();
         unordered_map<string, vector<string>> mp;
-        
-        for(auto x: strs){
-            string word = x;
-            sort(word.begin(), word.end());
-            mp[word].push_back(x);
+        for (int i = 0; i < n; i++) {
+            string t = strs[i];
+            sort(t.begin(), t.end());
+            mp[t].push_back(strs[i]);
         }
-        
-        vector<vector<string>> ans;
-        for(auto x: mp){
-            ans.push_back(x.second);
+        vector<vector<string>> res;
+        for (auto it : mp) {
+            res.push_back(it.second);
         }
-        return ans;
+        return res;
+    }
+    vector<vector<string>> optimal(vector<string>& v) {
+        int n = v.size();
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> res;
+
+        for (int i = 0; i < n; i++) {
+            string s = v[i];
+            vector<int> fre(26, 0);
+            for (int j = 0; j < s.size(); j++) {
+                fre[s[j] - 'a']++;
+            }
+
+            string temp = "";
+            for (int i = 0; i < 26; i++) {
+                temp += to_string(fre[i]) + "#";
+            }
+            mp[temp].push_back(s);
+        }
+        for(auto it:mp){
+            res.push_back(it.second);
+        }
+        return res;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // return better(strs);
+        return optimal(strs);
     }
 };
