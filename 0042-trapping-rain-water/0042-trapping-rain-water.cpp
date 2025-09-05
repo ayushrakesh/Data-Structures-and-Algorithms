d1e52c5343f122bf975f0ed500a7043b8040dd13
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-09-05 08:52:03 +0530

    Time: 0 ms (100%), Space: 26 MB (79.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
+    int better(vector<int>& height) {
         int n = height.size();
         int res = 0;
         vector<int> pre(n), suf(n);
@@ -8,12 +8,12 @@ public:
         int a = height[0];
         for (int i = 0; i < n; i++) {
             pre[i] = max(a, height[i]);
-            a=pre[i];
+            a = pre[i];
         }
         int b = height[n - 1];
         for (int i = n - 1; i >= 0; i--) {
             suf[i] = max(b, height[i]);
-            b=suf[i];
+            b = suf[i];
         }
         for (int i = 0; i < n; i++) {
             if (pre[i] > height[i] && height[i] < suf[i]) {
@@ -21,5 +21,29 @@ public:
             }
         }
         return res;
+    }
+    int optimal(vector<int>&height){
+        int n=height.size();
+        int i=0,j=n-1;
+        int lmax=0,rmax=0;
+        int res=0;
+        while(i<=j){
+            lmax=max(lmax,height[i]);
+            rmax=max(rmax,height[j]);
+
+            if(lmax<=rmax){
+                res+=lmax-height[i];
+                i++;
+            }
+            else{
+                res+=rmax-height[j];
+                j--;
+            }
+        }
+        return res;
+    }
+    int trap(vector<int>& height) {
+        // return better(height);
+        return optimal(height);
     }
 };
