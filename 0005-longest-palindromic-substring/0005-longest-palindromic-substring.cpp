d83def7f057134eb76f712b29785e3f425302f04
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-09-06 11:33:40 +0530

    Time: 206 ms (24.19%), Space: 291.9 MB (9.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -5,7 +5,6 @@ public:
         string res = "";
 
         for (int i = 0; i < n; i++) {
-            // Odd length palindrome
             int low = i, high = i;
             while (low >= 0 && high < n && s[low] == s[high]) {
                 low--;
@@ -17,7 +16,6 @@ public:
                 res = s.substr(low, high - low + 1);
             }
 
-            // Even length palindrome
             low = i, high = i + 1;
             while (low >= 0 && high < n && s[low] == s[high]) {
                 low--;
@@ -31,8 +29,33 @@ public:
         }
         return res;
     }
+    string better(string s){
+        int n=s.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        int maxlen=1;
+        int start=0;
 
+        for(int i=0;i<n;i++)dp[i][i]=1;
+
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<n;i++){
+                int j=i+len-1;
+
+                if(j<n && s[i]==s[j]){
+                    if(dp[i+1][j-1]==1 || len==2){
+                        dp[i][j]=1;
+                        if(maxlen < len){
+                            maxlen=len;
+                            start=i;
+                        }
+                    }
+                }
+            }
+        }
+        return s.substr(start,maxlen);
+    }
     string longestPalindrome(string s) {
-        return brute(s);
+        // return optimal(s);
+        return better(s);
     }
 };
