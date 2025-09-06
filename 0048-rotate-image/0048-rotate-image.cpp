c55b8aa932b6d73c55070ced4320cbd7ae323cce
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-09-06 10:11:55 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,7 +3,6 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         for (int i = 0; i < matrix.size(); i++) {
             for (int j = i + 1; j < matrix[0].size(); j++) {
-                /* code */
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
