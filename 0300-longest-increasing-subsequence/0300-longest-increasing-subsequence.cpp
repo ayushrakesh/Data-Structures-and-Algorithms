a25e2338b6d2b05bb7eabcfeed652f694e69ccdf
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-09-07 19:22:25 +0530

    Time: 285 ms (35.05%), Space: 296.5 MB (13.69%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
+    int solve(int ind, int last, vector<int>& nums, vector<vector<int>>& dp) {
+        if (ind >= nums.size())
+            return 0;
 
-    int solve(int last, int ind, vector<int> nums, vector<vector<int>>& dp) {
-    
-        if (ind == nums.size()) return 0;
-        if (dp[ind][last+1] != -1) return dp[ind][last+1];
-        int take=0;
-        if (last == -1 || nums[ind] > nums[last]) take=1+solve(ind, ind+1, nums, dp);
-        int nottake=solve(last, ind+1, nums, dp);
-        return dp[ind][last+1]=max(take, nottake);
-    }
-    int tab(vector<int> nums,vector<vector<int>> &dp){
-        int n=nums.size();
-        for(int i=0;i<=n;i++){
-            dp[n][i]=0;
+        if (dp[ind][last + 1] != -1)
+            return dp[ind][last + 1];
+        int take = 0;
+        if (last == -1 || nums[ind] > nums[last]) {
+            take = solve(ind + 1, ind, nums, dp) + 1;
         }
-        for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=-1;j--){
-                int take=0;
-                if(j==-1 || nums[i]>nums[j]){
-                    take=1+dp[i+1][i+1];
+
+        int nottake = solve(ind + 1, last, nums, dp);
+        return dp[ind][last + 1] = max(take, nottake);
+    }
+    int tab(vector<int>& nums, vector<vector<int>>& dp) {
+        int n = nums.size();
+
+        for (int i = -1; i < n; i++) {
+            dp[n][i+1] = 0;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = -1; j < i; j++) {
+                int take = 0;
+                if (j == -1 || nums[i] > nums[j]) {
+                    take = dp[i + 1][i+1] + 1;
                 }
-                dp[i][j+1]=max(take,dp[i+1][j+1]);
+                int nottake = dp[i + 1][j+1];
+                dp[i][j+1] = max(take, nottake);
             }
         }
-        return dp[0][-1+1];
-    }    
+        return dp[0][0];
+    }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,-1)); 
-        // return solve(-1, 0, nums, dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        // return solve(0, -1, nums, dp)
         return tab(nums,dp);
     }
 };
