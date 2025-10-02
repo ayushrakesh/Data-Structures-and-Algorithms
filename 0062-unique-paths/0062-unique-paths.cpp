b19914c9e05c0d88bc9aac50779b661877f29187
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-10-02 19:05:07 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,8 +10,18 @@ public:
         return dp[row][col] = solve(row + 1, col, m, n, dp) +
                               solve(row, col + 1, m, n, dp);
     }
-    int tab(int m,int n,vector<vector<int>>&dp){
-        
+    int tab(int m, int n, vector<vector<int>>& dp) {
+        for (int i = 0; i <= n; i++)
+            dp[m][i] = 0;
+        for (int i = 0; i <= m; i++)
+            dp[i][n] = 0;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+        return dp[0][0];
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
