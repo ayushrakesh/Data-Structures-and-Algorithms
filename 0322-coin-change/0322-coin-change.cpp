4e0ffe95671d003f36cf771e3fa2a9020e0d75f2
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-10-04 23:03:26 +0530

    Time: 43 ms (44.21%), Space: 48.8 MB (42.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-    long long solve(int ind, int amount, vector<int>& coins,
-                    vector<vector<int>>& dp) {
-        int n = coins.size();
-
-        if (ind == n - 1) {
-            if (amount % coins[n - 1] == 0)
-                return amount / coins[n - 1];
-            return INT_MAX;
+    int solve(int ind , int target, vector<int> arr, vector<vector<int>>& dp) {
+        int n=arr.size();
+        if (ind == 0) {
+            if (target % arr[ind] == 0) return target/arr[ind];
+            else return 1e8;
         }
-        if (dp[ind][amount] != -1)
-            return dp[ind][amount];
-
-        long long nottake = solve(ind + 1, amount, coins, dp);
-        long long take = INT_MAX;
-
-        if (coins[ind] <= amount) {
-            take = 1 + solve(ind, amount - coins[ind], coins, dp);
+        
+        if (dp[ind][target]!=-1) return dp[ind][target];
+        
+        int pick=1e8;
+        if (target >= arr[ind]) {
+            pick = 1 + solve(ind, target-arr[ind], arr, dp);
         }
-        return dp[ind][amount] = min(take, nottake);
+        int notpick = solve(ind-1, target, arr, dp);
+        
+        return dp[ind][target] = min(pick , notpick);
+        // return min(pick , notpick);
+    }
+    int tab(int ind,int target,vector<int> &arr,vector<vector<int>> &dp){
+        int n=arr.size();
+        for(int i=0;i<=target;i++){
+            if(i % arr[0]==0){
+                dp[0][i]=i/arr[0];
+            }
+            else dp[0][i]=1e8;
+        }
+        
+        for(int i=1;i<=ind;i++){
+            for(int j=0;j<=target;j++){
+                if(arr[i]<=j){
+                    dp[i][j]=min(dp[i-1][j], 1 + dp[i][j-arr[i]]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[ind][target];
     }
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-
-        int res = ((int)solve(0, amount, coins, dp));
-        return res == INT_MAX ? -1 : res;
+        int n=coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int res = tab(n-1, amount, coins, dp);
+        if (res == 1e8) return -1;
+        return res;
     }
 };
