9d76d26d04ba448b0e132bdc71361a7ab8333754
Ayush Rakesh <115995339+ayushrakesh@users.noreply.github.com>
2025-10-06 09:18:10 +0530

    Time: 68 ms (7.73%), Space: 82.6 MB (7.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int i, int j, string s, string t) {
+    int solve(int i, int j, string s, string t, vector<vector<int>>& dp) {
         int n = s.size();
         int m = t.size();
 
@@ -9,15 +9,20 @@ public:
         if (j >= m)
             return n - i;
 
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
         if (s[i] == t[j])
-            return solve(i + 1, j + 1, s, t);
-        return 1 + min(solve(i, j + 1, s, t),
-                       min(solve(i + 1, j, s, t), solve(i + 1, j + 1, s, t)));
+            return dp[i][j] = solve(i + 1, j + 1, s, t, dp);
+        return dp[i][j] = 1 + min(solve(i, j + 1, s, t, dp),
+                                  min(solve(i + 1, j, s, t, dp),
+                                      solve(i + 1, j + 1, s, t, dp)));
     }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
 
-        return solve(0, 0, word1, word2);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return solve(0, 0, word1, word2, dp);
     }
 };
